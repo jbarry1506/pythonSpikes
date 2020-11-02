f12d0542a3e4bc88ec7ab164fe33afc25618322d
Jim Barry <jbarry1506@gmail.com>
2020-11-02 16:21:56 -0500

    pysmb_practice

--- pysmb_practice.py
+++ pysmb_practice.py
@@ -1,8 +1,19 @@
-import pysmb
+from smb.SMBConnection import SMBConnection
+import pprint
 import vars
 
+userID = vars.beast_un
+password = vars.beast_xv
+client_machine_name = 'pi3web'
 
-conn = SMBConnection(user=vars.beast_un, pw=vars.beast_xv, my_name="pi3web", srv, use_ntlm_v2=True)
-conn.connect(ip, port=139)
-file2transfer = open(filename, "r")
-conn.storeFile(share, path+filename, file2transfer, timeout=30)
+server_name = vars.beast_hostname
+server_ip = vars.beast_ip
+
+domain_name = ''
+
+
+conn = SMBConnection(userID, password, client_machine_name, server_name, domain=domain_name, use_ntlm_v2=True, is_direct_tcp=True)
+conn.connect(vars.beast_ip, port=445)
+pprint.pprint(conn)
+# file2transfer = open(filename, "r")
+# conn.storeFile(share, path+filename, file2transfer, timeout=30)
