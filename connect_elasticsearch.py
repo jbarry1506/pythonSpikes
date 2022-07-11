0f364f0bf1393aafb0c083f60691c5067e5ac4a0
Jim Barry <jbarry1506@gmail.com>
2022-07-11 05:38:20 -0400

    elasticsearch module play

--- connect_elasticsearch.py
+++ connect_elasticsearch.py
@@ -1,11 +1,21 @@
+import elasticsearch
 import requests
 import creds
 import json
-
+from elasticsearch import Elasticsearch
 
 server = creds.elasticsearch_server
-table = "/movies"
-headers = {'Content-Type': 'application/json'}
+print(elasticsearch.__version__)
+es = Elasticsearch(creds.elasticsearch_server)
+print(es.ping())
+
+# es.indices.create(index="bible")
+print(es.indices.exists(index="bible"))
+
+print(es.indices.get_alias(index="*"))
+
+# table = "/movies"
+# headers = {'Content-Type': 'application/json'}
 
 #--------------------------------------
 # THIS WORKS
@@ -16,9 +26,9 @@ headers = {'Content-Type': 'application/json'}
 #--------------------------------------
 
 # this will give information about the indices of the elasticsearch
-param = (('v', ''),)
-resp = requests.get(f'{server}/_cat/indices', params=param)
-print(resp.text)
+# param = (('v', ''),)
+# resp = requests.get(f'{server}/_cat/indices', params=param)
+# print(resp.text)
 
 #--------------------------------------
 
@@ -43,17 +53,29 @@ print(resp.text)
 # print(resp.text)
 
 #----------------------------------------
-table = "/shakespeare"
-elastic_url_switch = "/_search?pretty"
+# table = "/shakespeare"
+# elastic_url_switch = "/_search?pretty"
 
-data = {
-    "query": {
-        "match_phrase": {
-            "text_entry": "but soft"
-        }
-    }
-}
+# data = {
+#     "query": {
+#         "match_phrase": {
+#             "text_entry": "but soft"
+#         }
+#     }
+# }
 
-jdata = json.dumps(data)
-resp = requests.get(server+table+elastic_url_switch, headers=headers, data=jdata)
-print(resp.text)
+# jdata = json.dumps(data)
+# resp = requests.get(server+table+elastic_url_switch, headers=headers, data=jdata)
+# print(resp.text)
+
+#----------------------------------------
+
+# Create new index
+# server = creds.elasticsearch_server
+# table = "/bible"
+# headers = {'Content-Type': 'application/json'}
+
+#----------------------------------------
+
+# Fix yellow status if replicas > 0
+
