4e84683fda601dffb5d04c907df2544961749a5e
Jim Barry <jbarry1506@gmail.com>
2022-07-15 00:50:13 -0400

    started oakd_snap_pic

---
+++ oakd_snap_pic.py
@@ -0,0 +1,20 @@
+import cv2
+import depthai as dai
+
+
+# check to see if the camera has a command to capture a still
+daicam = dai.CameraControl()
+still = daicam.getCaptureStill()
+
+if still:
+    pass
+else:
+    print(f"There is no command to capture a still: {still}",
+          "\nFixing it!")
+    #
+    daicam_truth = daicam.setCaptureStill(True)
+    print(f"This is the daicam_truth:  {daicam_truth}!")
+    print(f"This is what we started with:  {still}")
+    still = daicam_truth
+    print(f"Now this is what we got:  {still}")
+
