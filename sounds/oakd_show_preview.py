d5ddebfb11d642a4cf5847653282b3fff187122b
Jim Barry <jbarry1506@gmail.com>
2022-07-30 12:13:38 -0400

    oakd_show_preview will snap pic on command

--- sounds/oakd_show_preview.py
+++ sounds/oakd_show_preview.py
@@ -1,34 +1,26 @@
-from sys import maxsize
-import time
-from pathlib import Path
+
 import cv2
 import depthai as dai
-
+import time
 
 # create pipeline
 pipeline = dai.Pipeline()
 
 # define sources and outputs
 camRgb = pipeline.create(dai.node.ColorCamera)
-
-
 xoutRgb = pipeline.create(dai.node.XLinkOut)
-controlIn = pipeline.create(dai.node.XLinkIn)
 
 xoutRgb.setStreamName("rgb")
-controlIn.setStreamName("control")
 
 #properties
 # cam preview settings
-camRgb.setPreviewSize(300,300)
+camRgb.setPreviewSize(1080,720)
 camRgb.setInterleaved(False)
-
-camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
-camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
 
 # Linking
-camRgb.still.link(xoutRgb.input)
-controlIn.out.link(camRgb.inputControl)
+camRgb.preview.link(xoutRgb.input)
 
 # connect to device and start pipeline
 with dai.Device(pipeline) as device:
@@ -39,13 +31,10 @@ with dai.Device(pipeline) as device:
 
     # Output queue will be used to get the rgb frames from the output defined above
     qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-    controlQueue = device.getInputQueue("control")
 
     # make sure the destination path is present before starting to store the pictures
     dirname = "/home/jbarry1506/Pictures/oak-d/astronomy"
     # Path(dirname).mkdir(parents=True, exist_ok=True)
-
-    start_time = time.time()
 
     while True:
         inRgb = qRgb.get() # non-blocking call, will return new data or None otherwise
@@ -58,11 +47,4 @@ with dai.Device(pipeline) as device:
             ctrl = dai.CameraControl()
             ctrl.setCaptureStill(True)
             print(f"The capture mode is {ctrl.getCaptureStill()}")
-            cv2.imwrite(f"{dirname}/{int(time.time() * 1000)}.png", inRgb.getCvFrame())
-
-            controlQueue.send(ctrl)
-
-        # else:
-
-        #     time.sleep(10)
-            
+            cv2.imwrite(f"{dirname}/{int(time.time() * 1000)}.png", inRgb.getCvFrame())            
